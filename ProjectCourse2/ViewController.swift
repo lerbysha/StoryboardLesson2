7da6617674d6b05e7f17c4f316dfa006cc75aeb7
Ildar Zalyalov <ildarzalyalovmos@gmail.com>
2018-09-18 10:13:46 +0300

    lesson commit

--- ProjectCourse2/ViewController.swift
+++ ProjectCourse2/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    var someArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        someArray = ["some value", "value1"]
+        
+        myLabel.text = someArray[1]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
